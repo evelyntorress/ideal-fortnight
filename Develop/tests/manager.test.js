b922fd7d52066095000b599dfc87ac9c961bd93d
Evelyn Torres <evelyn@MacBook-Air.hitronhub.home>
2022-06-13 14:47:39 -0700

    first commit

---
+++ Develop/tests/manager.test.js
@@ -0,0 +1,27 @@
+class Manager {
+    constructor(name, id, email, officeNumber) {        
+     this.name = name;
+     this.id = id;
+     this.email = email;
+     this.officeNumber = officeNumber;
+    }
+
+getName() {
+ return this.name;
+}
+
+getId(){
+ return this.id;
+}
+
+getEmail(){
+ return this.email;
+}
+
+getofficeNumber(){
+ return this.officenumber;
+   }
+
+getRole(){
+ return "Manager";
+}
